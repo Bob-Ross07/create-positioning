7abc868005df3f5582e65eff1a2a5afaa2f35a76
Bob-Ross07 <128514351+Bob-Ross07@users.noreply.github.com>
2023-05-01 14:44:24 -0400

    Create create_worlds.c

---
+++ create_worlds.c
@@ -0,0 +1,20 @@
+#include <kipr/wombat.h>
+int arm1;
+int arm2;
+int up;
+
+int claw;
+int wrist;
+int open;
+int close;
+
+//wrist values for different tower heights
+int low;
+int high;
+
+
+
+//drive forward a given distance while setting up the arm to grab cube/botgal
+void forward(int distance) {
+  
+}
