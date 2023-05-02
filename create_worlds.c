7a40793ab9d1b27e02fead45523e2ecd3af0075c
Bob-Ross07 <128514351+Bob-Ross07@users.noreply.github.com>
2023-05-02 11:14:02 -0400

    Update create_worlds.c

--- create_worlds.c
+++ create_worlds.c
@@ -12,11 +12,30 @@ int close;
 int low;
 int high;
 
+int rspeed;
+int lspeed;
+
 
 
 //drive forward a given distance while setting up the arm to grab cube/botgal
-void forward(int distance) {
-  while(get_create_total_distance() < distance*10) {
-
+void forward_set(int distance) {
+  set_servo_position(claw, open);
+  msleep(100);
+  set_create_total_distance(0);
+  while(get_create_total_distance() < distance*10){
+    create_drive_direct(rspeed, lspeed);
+    if(gmpc(arm1) < up-50) {
+      mav(arm1, upspeed);
+      mav(arm2, upspeed);
+      
+      msleep(10);
+    }
+    
+    else if(gmpc(arm1) < up+50) {
+      mav(arm1, upspeed);
+      mav(arm2, upspeed);
+      msleep(10);
+    }
+  }
 }
-}
+  
